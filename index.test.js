6d489baeb48182d3721ec3e7b0db5b7a094714fd
Estefani <stef45689@gmail.com>
2021-11-05 12:50:06 -0500

    tried something but it's failing

--- index.test.js
+++ index.test.js
@@ -2,7 +2,9 @@ const getMethod = require('./index.js');
 
 describe('the GET method', () => {
   it('it gets an order in the db by id', () => {
-    const getMethod = `GET`;
-    expect(getMethod).toEqual('GET');
+    const method = `GET / HTTP/1.1
+    Host: example.com
+    Accept-Language: us-en`;
+    expect(method.split[0]).toEqual('GET');
   });
 });
