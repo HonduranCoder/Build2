60a1ec62111fbedb2e4fe0391831f6809f0b1b8f
Estefani <stef45689@gmail.com>
2021-11-05 12:54:30 -0500

    Blindly breaking things and getting different errors

--- index.test.js
+++ index.test.js
@@ -2,9 +2,7 @@ const getMethod = require('./index.js');
 
 describe('the GET method', () => {
   it('GET method', () => {
-    const method = `GET / HTTP/1.1
-    Host: example.com
-    Accept-Language: us-en`;
-    expect(method.split[0]).toEqual('GET');
+    const method = getMethod(method.split[0]);
+    expect(method).toEqual(GET);
   });
 });
